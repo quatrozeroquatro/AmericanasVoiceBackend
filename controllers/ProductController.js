14ace629224ceb2c89835bb0596250dc7827eabd
Yasmin Romi <yasmin.romi@radixeng.com.br>
2020-05-03 09:13:29 -0300

    fixing search products v2

--- controllers/ProductController.js
+++ controllers/ProductController.js
@@ -3,17 +3,18 @@ const ProductService = require('../services/ProductService');
 class ProductController {
     async searchProduct(req, res)     {
         const { product } = req.params;
-        const response = await ProductService.searchProductsV1Service(product)
-            .then(items =>
-                items.map(item => await ProductService.searchProductsV2Service(item.id).then(secondResponse => {
-                    const product = {
-                        image: secondResponse.data.product.result.images[0].medium,
-                        name: secondResponse.data.product.result.name,
-                        price: secondResponse.data.offer.result.offers[0].salesPrice,
-                        store: secondResponse.data.offer.result.offers[0]._embedded.seller.name
-                    }
-                    return product;
-                })));
+        const products = await ProductService.searchProductsV1Service(product);
+        const productsDetails = products.map(item => await ProductService.searchProductsV2Service(item.id));
+        
+        response = productsDetails.map(item => {
+            const product = {
+                image: item.product.result.images[0].medium,
+                name: item.product.result.name,
+                price: item.offer.result.offers[0].salesPrice,
+                store: item.offer.result.offers[0]._embedded.seller.name
+            }
+            return product;
+        })
         
         console.log(response);
         return res.json(response);
