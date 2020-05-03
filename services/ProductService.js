54847c1cf9a48f5a7e1cb5873c58bd4a7042d3c3
Yasmin Romi <yasmin.romi@radixeng.com.br>
2020-05-03 10:49:15 -0300

    fixing search products v2

--- services/ProductService.js
+++ services/ProductService.js
@@ -9,6 +9,16 @@ class ProductService {
                 sortBy: 'lowerPrice',
                 source: 'nanook',
                 limit: 2
+            },
+            headers: { 
+                "accept": "application/json",
+                "accept-language": "en-US,en;q=0.9,pt-BR;q=0.8,pt;q=0.7",
+                "sec-fetch-dest": "empty",
+                "sec-fetch-mode": "cors",
+                "sec-fetch-site": "cross-site",
+                "referrer": "https://www.americanas.com.br/produto/1509599829/coqueteleira-iridium-labs?cor=Amarela&pfm_carac=coqueteleira&pfm_page=search&pfm_pos=grid&pfm_type=search_page",
+                "referrerPolicy": "no-referrer-when-downgrade",
+                "mode": "cors"
             }
         }).then(response => {
             return response.data.products;
