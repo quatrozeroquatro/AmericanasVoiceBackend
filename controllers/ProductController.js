e36f5b61300b903b55c1853c4f568155a8b78748
Yasmin Romi <yasmin.romi@radixeng.com.br>
2020-05-03 09:42:54 -0300

    fixing search products v2

--- controllers/ProductController.js
+++ controllers/ProductController.js
@@ -4,20 +4,28 @@ class ProductController {
     async searchProduct(req, res)     {
         const { product } = req.params;
         const products = await ProductService.searchProductsV1Service(product);
-        const productsDetails = await Promise.all(products.map(item => ProductService.searchProductsV2Service(item.id)));
+        //const productsDetails = await Promise.all(products.map(item => ProductService.searchProductsV2Service(item.id)));
+
+        // response = productsDetails.map(item => {
+        //     const product = {
+        //         image: item.product.result.images[0].medium,
+        //         name: item.product.result.name,
+        //         price: item.offer.result.offers[0].salesPrice,
+        //         store: item.offer.result.offers[0]._embedded.seller.name
+        //     }
+        //     return product;
+        // })
+
+        const productsDetails = await ProductService.searchProductsV2Service(products[0].id);
+        const product = {
+            image: productsDetails.product.result.images[0].medium,
+            name: productsDetails.product.result.name,
+            price: productsDetails.offer.result.offers[0].salesPrice,
+            store: productsDetails.offer.result.offers[0]._embedded.seller.name
+        }
         
-        response = productsDetails.map(item => {
-            const product = {
-                image: item.product.result.images[0].medium,
-                name: item.product.result.name,
-                price: item.offer.result.offers[0].salesPrice,
-                store: item.offer.result.offers[0]._embedded.seller.name
-            }
-            return product;
-        })
-        
-        console.log(response);
-        return res.json(response);
+        console.log(product);
+        return res.json(product);
     }
 }
 
